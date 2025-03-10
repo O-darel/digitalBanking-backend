94d4d61b70ae24cfa7075330a75e631dc494520a
Darel <oballa99darel@gmail.com>
2025-03-10 13:43:26 +0300

    Project structure

---
+++ audit-logging-service/src/main/java/com/bank/audit_logging_service/AuditLoggingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bank.audit_logging_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditLoggingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditLoggingServiceApplication.class, args);
+	}
+
+}
