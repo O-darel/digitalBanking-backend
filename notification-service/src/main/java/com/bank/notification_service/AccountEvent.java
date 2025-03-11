c32863d3d571551e4eb22f016f2991c7513eadd9
ErickKaranja <karanja99erick.com>
2025-03-11 20:51:27 +0300

    upated kafka consumer

---
+++ notification-service/src/main/java/com/bank/notification_service/AccountEvent.java
@@ -0,0 +1,14 @@
+package com.bank.notification_service;
+
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountEvent {
+    private String accountNumber;
+    private String email;
+}
